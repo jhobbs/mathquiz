edb0f3cdf93b97f0af5289b0835bc21298dae679
Jason Hobbs <jason.hobbs@gmail.com>
2015-03-01 21:14:39 -0600

    Make quiz an object.

--- mathquiz/quiz.py
+++ mathquiz/quiz.py
@@ -1,20 +1,39 @@
 import random
 
 from argparse import ArgumentParser
+from collections import namedtuple
 
-from mathquiz.questions import questions
+from mathquiz.questions import questions as quests
+
+UserRecord = namedtuple('UserRecord', [
+    'name',
+    'results',
+    ])
+
+
+class Quiz(object):
+    def __init__(self, questions, user):
+        self.questions = questions
+        self.user = user
+
+    def run(self, num_questions):
+        num_correct = sum([
+            random.choice(self.questions)().ask()
+            for _ in xrange(num_questions)])
+        
+        print("You got %d out of %d questions right!" % (
+            num_correct, num_questions))
+
+
+def parse_args(argv):
+    parser = ArgumentParser(description="Enjoy a math quiz.")
+    parser.add_argument("-n", "--num_questions",
+        help="Number of questions in the quiz.", default=10, type=int)
+    parser.add_argument("user", help="Name of user.")
+    return parser.parse_args(argv[1:])
+
 
 def main(argv):
-    parser = ArgumentParser(description="Enjoy a math quiz.")
-    parser.add_argument("--num_questions",
-        help="Number of questions in the quiz.", default=10, type=int)
-    args = parser.parse_args(argv[1:])
-    run_quiz(args.num_questions)
-
-
-def run_quiz(num_questions):
-    num_correct = sum([
-        random.choice(questions)().ask()
-        for _ in xrange(num_questions)])
-    
-    print("You got %d out of %d questions right!" % (num_correct, num_questions))
+    args = parse_args(argv)
+    quiz = Quiz(quests, args.user)
+    quiz.run(args.num_questions)
