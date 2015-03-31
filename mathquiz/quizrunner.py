1245cbf69141fa6c96f3e4f40753d5bfd437180e
Jason Hobbs <jason.hobbs@gmail.com>
2015-03-30 20:38:32 -0500

    Split QuizRunner into a separate file.

---
+++ mathquiz/quizrunner.py
@@ -0,0 +1,112 @@
+import random
+
+from argparse import ArgumentParser
+from collections import namedtuple
+
+from mathquiz.quiz import Quiz
+from mathquiz.questions import builtin_question_types
+from mathquiz.storage import store_quiz_results_local
+
+
+good_names = [
+    'genius',
+    'whizkid',
+    'smarty pants',
+    'Doctor',
+    'master',
+    'winner',
+    'your awesomeness',
+    ]
+
+bad_names = [
+    'doofus',
+    'cretin',
+    'loser',
+    'half-wit',
+    'meathead',
+    'imbecile',
+    'chump',
+    ]
+
+
+QuizResult = namedtuple('QuizResult', [
+    'correct',
+    'total',
+    ])
+
+
+def print_quiz_result(quiz_result): 
+    print("You got %d out of %d questions right!" % (
+        quiz_result.correct, quiz_result.total))
+
+
+def add_argument_from_option(parser, module_name, option):
+    parser.add_argument(
+        "--%s-%s" % (module_name, option['name']),
+        help=option['help'],
+        default=option['default'],
+        type=option.get('type', str),
+        )
+
+
+class ConsoleQuizRunner(object):
+    def __init__(self, question_types):
+        self.question_types = question_types
+
+    def run(self, argv):
+        self.parse_args(argv)
+        if self.args.include is not None:
+            question_types = [
+                question_type for question_type
+                in self.question_types
+                if question_type.name in self.args.include]
+        else:
+            question_types = self.question_types
+        quiz = Quiz(question_types)
+        results = self.run_quiz(quiz)
+        store_quiz_results_local(self.args.user, results)
+
+        print_quiz_result(results)
+
+    def run_quiz(self, quiz):
+        num_correct = sum([
+                self.ask_question(question)
+                for question in quiz.questions(self.args)
+            ])
+        return QuizResult(correct=num_correct, total=self.args.num_questions)
+
+    def parse_args(self, argv):
+        parser = ArgumentParser(description="Enjoy a math quiz.")
+        parser.add_argument("-n", "--num_questions",
+            help="Number of questions in the quiz.", default=10, type=int)
+        parser.add_argument("-i", "--include",
+            nargs="+",
+            help="questions to include. by default, all are included, but if this is specified only those specified are included.")
+        self.add_question_args(parser)
+        parser.add_argument("user", help="Name of user.")
+        self.args = parser.parse_args(argv[1:])
+
+    def add_question_args(self, parser):
+        for question_type in self.question_types:
+            for option_name, option in question_type.options.iteritems():
+                option['name'] = option_name
+                add_argument_from_option(parser, question_type.name, option)
+
+    def ask_question(self, question):
+        print(question.explain())
+        answer = raw_input(question.question_string())
+        if not question.check_answer(answer):
+            print "Wrong, %s! The correct answer is: %s" % (
+                random.choice(bad_names), question.answer)
+            return 0
+        print "Correct, %s!" % (random.choice(good_names))
+        return 1
+
+
+def run_quiz(argv):
+    runner = ConsoleQuizRunner(builtin_question_types)
+    results = runner.run(argv)
+
+
+def main(argv):
+    run_quiz(argv)
