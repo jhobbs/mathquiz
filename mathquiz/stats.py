4e36093a4706375f76927f63d3f59572de090e55
Jason Hobbs <jason.hobbs@gmail.com>
2015-04-03 18:50:27 -0500

    Add display of failures grouped by type.

--- mathquiz/stats.py
+++ mathquiz/stats.py
@@ -1,4 +1,11 @@
+from collections import defaultdict
 from mathquiz.storage import get_current_user_data
+
+def group_by_type(question_results):
+    grouped = defaultdict(list)
+    for question_result in question_results:
+        grouped[question_result.question.name].append(question_result)
+    return grouped
 
 
 def display_stats(args):
@@ -17,6 +24,11 @@ def display_stats(args):
     print("Correctly answered questions: %d" % len(correct_questions))
     print("Overall success rate: %d%%" % (
         int(float(len(correct_questions))/len(questions) * 100)))
+    incorrect_questions = set(questions).difference(correct_questions)
+    failures_grouped_by_type = group_by_type(incorrect_questions)
+    print("Number of incorrect answers for each question type: ")
+    for failure_type, failures in failures_grouped_by_type.iteritems():
+        print("%s: %d failures" % (failure_type, len(failures)))
 
 
 def setup_parser(parser):
