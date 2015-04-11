1619d859c2214fc9ddda2d2febf14da45f3b0325
Jason Hobbs <jason.hobbs@gmail.com>
2015-04-10 19:04:52 -0500

    Ask unmastered questions 5 times as often as mastered ones.

--- mathquiz/quiz.py
+++ mathquiz/quiz.py
@@ -6,20 +6,29 @@ from mathquiz.stats import (
     )
 
 
+UNMASTERED_BOOST = 5
+
+
 class Quiz(object):
     def __init__(self, question_types, user_data):
         self.question_types = question_types
         self.user_data = user_data
         self.weighted_question_types = self.get_weighted_question_types()
+        print self.weighted_question_types
 
     def get_weighted_question_types(self):
         question_history = questions_from_user_data(self.user_data)
         question_types_by_mastery = group_by_mastery(
             self.question_types, question_history)
-        return self.question_types
+        weighted_questions = []
+        for question_type in question_types_by_mastery['mastered']:
+            weighted_questions.append(question_type)
+        for question_type in question_types_by_mastery['unmastered']:
+            weighted_questions.extend([question_type] * UNMASTERED_BOOST) 
+        return weighted_questions
 
     def pick_next_question_type(self):
-        question_type = random.choice(self.question_types)
+        question_type = random.choice(self.weighted_question_types)
         return question_type
 
     def questions(self, options):
